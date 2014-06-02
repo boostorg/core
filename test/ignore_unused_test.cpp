70a9ef7141998eb63a1ffe2c6288626f3cfed287
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2014-06-02 15:18:23 +0200

    Fix #include in ignore_unused_test.cpp

--- test/ignore_unused_test.cpp
+++ test/ignore_unused_test.cpp
@@ -4,8 +4,7 @@
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-//#include <boost/utility/ignore_unused.hpp>
-#include <boost/utility.hpp>
+#include <boost/core/ignore_unused.hpp>
 
 int main()
 {
