9092fde17ed8bf30b2564d884a774134b53ae127
Andrey Semashev <andrey.semashev@gmail.com>
2014-06-05 00:19:04 +0400

    Added docs for scoped_enum.hpp. Extracted underlying_type to a separate header.

--- include/boost/core/scoped_enum.hpp
+++ include/boost/core/scoped_enum.hpp
@@ -7,125 +7,10 @@
 //  Distributed under the Boost Software License, Version 1.0.
 //  See http://www.boost.org/LICENSE_1_0.txt
 
-/*
-[section:scoped_enums Scoped Enums]
-
-Generates C++0x scoped enums if the feature is present, otherwise emulates C++0x
-scoped enums with C++03 namespaces and enums. The Boost.Config BOOST_NO_CXX11_SCOPED_ENUMS
-macro is used to detect feature support.
-
-See http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2007/n2347.pdf for a
-description of the scoped enum feature. Note that the committee changed the name
-from strongly typed enum to scoped enum.
-
-Some of the enumerations defined in the standard library are scoped enums.
-
-  enum class future_errc
-  {
-      broken_promise,
-      future_already_retrieved,
-      promise_already_satisfied,
-      no_state
-  };
-
-On compilers that don't support them, the library provides two emulations:
-
-[heading Strict]
-
-* Able to specify the underlying type.
-* explicit conversion to/from underlying type.
-* The wrapper is not a C++03 enum type.
-
-The user can declare  declare these types as
-
-  BOOST_SCOPED_ENUM_DECLARE_BEGIN(future_errc)
-  {
-      broken_promise,
-      future_already_retrieved,
-      promise_already_satisfied,
-      no_state
-  }
-  BOOST_SCOPED_ENUM_DECLARE_END(future_errc)
-
-These macros allows to use 'future_errc' in almost all the cases as an scoped enum.
-
-  future_errc err = future_errc::no_state;
-
-There are however some limitations:
-
-* The type is not a C++ enum, so 'is_enum<future_errc>' will be false_type.
-* The emulated scoped enum can not be used in switch nor in template arguments. For these cases the user needs to use some macros.
-
-Instead of
-
-        switch (ev)
-        {
-        case future_errc::broken_promise:
-  // ...
-
-use
-
-        switch (boost::native_value(ev))
-        {
-        case future_errc::broken_promise:
-
-And instead of
-
-    #ifdef BOOST_NO_CXX11_SCOPED_ENUMS
-    template <>
-    struct BOOST_SYMBOL_VISIBLE is_error_code_enum<future_errc> : public true_type { };
-    #endif
-
-use
-
-    #ifdef BOOST_NO_CXX11_SCOPED_ENUMS
-    template <>
-    struct BOOST_SYMBOL_VISIBLE is_error_code_enum<future_errc::enum_type > : public true_type { };
-    #endif
-
-
-Sample usage:
-
-     BOOST_SCOPED_ENUM_UT_DECLARE_BEGIN(algae, char) { green, red, cyan }; BOOST_SCOPED_ENUM_DECLARE_END(algae)
-     ...
-     algae sample( algae::red );
-     void foo( algae color );
-     ...
-     sample = algae::green;
-     foo( algae::cyan );
-
- Light
-  Caution: only the syntax is emulated; the semantics are not emulated and
-  the syntax emulation doesn't include being able to specify the underlying
-  representation type.
-
-  The literal scoped emulation is via struct rather than namespace to allow use within classes.
-  Thanks to Andrey Semashev for pointing that out.
-  However the type is an real C++03 enum and so convertible implicitly to an int.
-
-  Sample usage:
-
-     BOOST_SCOPED_ENUM_START(algae) { green, red, cyan }; BOOST_SCOPED_ENUM_END
-     ...
-     BOOST_SCOPED_ENUM(algae) sample( algae::red );
-     void foo( BOOST_SCOPED_ENUM(algae) color );
-     ...
-     sample = algae::green;
-     foo( algae::cyan );
-
-  Helpful comments and suggestions were also made by Kjell Elster, Phil Endecott,
-  Joel Falcou, Mathias Gaunard, Felipe Magno de Almeida, Matt Calabrese, Vicente
-  Botet, and Daniel James.
-
-[endsect]
-*/
-
-
 #ifndef BOOST_CORE_SCOPED_ENUM_HPP
 #define BOOST_CORE_SCOPED_ENUM_HPP
 
 #include <boost/config.hpp>
-#include <boost/detail/workaround.hpp>
 
 #ifdef BOOST_HAS_PRAGMA_ONCE
 #pragma once
@@ -135,22 +20,6 @@ namespace boost
 {
 
 #ifdef BOOST_NO_CXX11_SCOPED_ENUMS
-  /**
-   * Meta-function to get the underlying type of a scoped enum.
-   *
-   * Requires EnumType must be an enum type or the emulation of a scoped enum
-   */
-  template <typename EnumType>
-  struct underlying_type
-  {
-    /**
-     * The member typedef type names the underlying type of EnumType. It is EnumType::underlying_type when the EnumType is an emulated scoped enum,
-     * std::underlying_type<EnumType>::type when the standard library std::underlying_type is provided.
-     *
-     * The user will need to specialize it when the compiler supports scoped enums but don't provides std::underlying_type.
-     */
-    typedef typename EnumType::underlying_type type;
-  };
 
   /**
    * Meta-function to get the native enum type associated to an enum class or its emulation.
@@ -200,12 +69,6 @@ namespace boost
 #else  // BOOST_NO_CXX11_SCOPED_ENUMS
 
   template <typename EnumType>
-  struct underlying_type
-  {
-    //typedef typename std::underlying_type<EnumType>::type type;
-  };
-
-  template <typename EnumType>
   struct native_type
   {
     typedef EnumType type;
@@ -222,9 +85,9 @@ namespace boost
   EnumType native_value(EnumType e)
   {
     return e;
- }
+  }
 
-#endif
+#endif // BOOST_NO_CXX11_SCOPED_ENUMS
 }
 
 
@@ -233,7 +96,7 @@ namespace boost
 #ifndef BOOST_NO_CXX11_EXPLICIT_CONVERSION_OPERATORS
 
 #define BOOST_SCOPED_ENUM_UT_DECLARE_CONVERSION_OPERATOR \
-     explicit operator underlying_type() const { return get_underlying_value_(); }
+     explicit operator underlying_type() const BOOST_NOEXCEPT { return get_underlying_value_(); }
 
 #else
 
@@ -249,10 +112,11 @@ namespace boost
  */
 #define BOOST_SCOPED_ENUM_UT_DECLARE_BEGIN(EnumType, UnderlyingType)    \
     struct EnumType {                                                   \
+        typedef void is_boost_scoped_enum_tag;                          \
         typedef UnderlyingType underlying_type;                         \
         EnumType() BOOST_NOEXCEPT {}                                    \
-        explicit EnumType(underlying_type v) : v_(v) {}                 \
-        underlying_type get_underlying_value_() const { return v_; }               \
+        explicit EnumType(underlying_type v) BOOST_NOEXCEPT : v_(v) {}                 \
+        underlying_type get_underlying_value_() const BOOST_NOEXCEPT { return v_; }    \
         BOOST_SCOPED_ENUM_UT_DECLARE_CONVERSION_OPERATOR                \
     private:                                                            \
         underlying_type v_;                                             \
@@ -299,19 +163,19 @@ namespace boost
 /**
  * Name of the native enum type.
  *
- * @param NT The new scoped enum.
+ * @param EnumType The new scoped enum.
  */
 #define BOOST_SCOPED_ENUM_NATIVE(EnumType) EnumType::enum_type
 /**
  * Forward declares an scoped enum.
  *
- * @param NT The scoped enum.
+ * @param EnumType The scoped enum.
  */
 #define BOOST_SCOPED_ENUM_FORWARD_DECLARE(EnumType) struct EnumType
 
 #else  // BOOST_NO_CXX11_SCOPED_ENUMS
 
-#define BOOST_SCOPED_ENUM_UT_DECLARE_BEGIN(EnumType,UnderlyingType) enum class EnumType:UnderlyingType
+#define BOOST_SCOPED_ENUM_UT_DECLARE_BEGIN(EnumType,UnderlyingType) enum class EnumType : UnderlyingType
 #define BOOST_SCOPED_ENUM_DECLARE_BEGIN(EnumType) enum class EnumType
 #define BOOST_SCOPED_ENUM_DECLARE_END2()
 #define BOOST_SCOPED_ENUM_DECLARE_END(EnumType) ;
@@ -321,21 +185,9 @@ namespace boost
 
 #endif  // BOOST_NO_CXX11_SCOPED_ENUMS
 
+// Deprecated macros
 #define BOOST_SCOPED_ENUM_START(name) BOOST_SCOPED_ENUM_DECLARE_BEGIN(name)
 #define BOOST_SCOPED_ENUM_END BOOST_SCOPED_ENUM_DECLARE_END2()
 #define BOOST_SCOPED_ENUM(name) BOOST_SCOPED_ENUM_NATIVE(name)
 
-//#ifdef BOOST_NO_CXX11_SCOPED_ENUMS
-//
-//# define BOOST_SCOPED_ENUM_START(name) struct name { enum enum_type
-//# define BOOST_SCOPED_ENUM_END };
-//# define BOOST_SCOPED_ENUM(name) name::enum_type
-//
-//#else
-//
-//# define BOOST_SCOPED_ENUM_START(name) enum class name
-//# define BOOST_SCOPED_ENUM_END
-//# define BOOST_SCOPED_ENUM(name) name
-//
-//#endif
 #endif  // BOOST_CORE_SCOPED_ENUM_HPP
