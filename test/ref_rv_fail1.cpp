78892a472a6d7917e0cdd8d496fe6cc1a200aced
Peter Dimov <pdimov@pdimov.com>
2014-06-10 03:26:09 +0300

    More cosmetic fixes in ref tests.

--- test/ref_rv_fail1.cpp
+++ test/ref_rv_fail1.cpp
@@ -1,8 +1,12 @@
+//
+// Test that a reference_wrapper can't be constructed from an rvalue
+//
 // Copyright 2014 Agustin Berge
 //
-// Distributed under the Boost Software License, Version 1.0. (See
-// accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt
+//
 
 #include <boost/ref.hpp>
 
@@ -10,7 +14,7 @@
 
 int main()
 {
-    boost::reference_wrapper<int const> r(1); // this should produce an ERROR
+    boost::reference_wrapper<int const> r( 1 ); // this should produce an ERROR
 }
 
 #else
--- test/ref_rv_fail3.cpp
+++ test/ref_rv_fail3.cpp
@@ -1,8 +1,12 @@
+//
+// Test that a const rvalue can't be passed to ref()
+//
 // Copyright 2014 Agustin Berge
 //
-// Distributed under the Boost Software License, Version 1.0. (See
-// accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt
+//
 
 #include <boost/ref.hpp>
 
@@ -14,7 +18,7 @@ X const crv() { return X(); }
 
 int main()
 {
-    boost::reference_wrapper<X const> r = boost::ref(crv()); // this should produce an ERROR
+    boost::reference_wrapper<X const> r = boost::ref( crv() ); // this should produce an ERROR
 }
 
 #else
