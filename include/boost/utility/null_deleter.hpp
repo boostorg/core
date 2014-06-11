a96e8b977bb8831bfef71f5fe248c5f01eaa5e8c
Andrey Semashev <andrey.semashev@gmail.com>
2014-06-12 02:10:10 +0400

    Added missing copyright comments. Corrected links to the documentation on the comments.

--- include/boost/utility/null_deleter.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef BOOST_UTILITY_NULL_DELETER_HPP
-#define BOOST_UTILITY_NULL_DELETER_HPP
-
-// The header file at this path is deprecated;
-// use boost/core/null_deleter.hpp instead.
-
-#include <boost/core/null_deleter.hpp>
-
-#endif
