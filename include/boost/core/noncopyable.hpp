a05906fd44adb1dec3c3cacab62b6f22be318ee6
Peter Dimov <pdimov@pdimov.com>
2018-08-25 15:15:53 +0300

    Add comment describing noncopyable_::base_token

--- include/boost/core/noncopyable.hpp
+++ include/boost/core/noncopyable.hpp
@@ -23,6 +23,14 @@ namespace noncopyable_  // protection from unintended ADL
 #ifndef BOOST_NONCOPYABLE_BASE_TOKEN_DEFINED
 #define BOOST_NONCOPYABLE_BASE_TOKEN_DEFINED
 
+// noncopyable derives from base_token to enable Type Traits to detect
+// whether a type derives from noncopyable without needing the definition
+// of noncopyable itself.
+//
+// The definition of base_token is macro-guarded so that Type Trais can
+// define it locally without including this header, to avoid a dependency
+// on Core.
+
   struct base_token {};
 
 #endif // #ifndef BOOST_NONCOPYABLE_BASE_TOKEN_DEFINED
