27715ed01a3e0bf0d0f9f26ddf4b70da23dc8c1d
Glen Fernandes <glen.fernandes@gmail.com>
2020-05-24 00:03:14 -0400

    Simplify alloc_construct using allocator_access

--- test/alloc_construct_cxx11_test.cpp
+++ test/alloc_construct_cxx11_test.cpp
@@ -6,7 +6,7 @@ Distributed under the Boost Software License, Version 1.0.
 (http://www.boost.org/LICENSE_1_0.txt)
 */
 #include <boost/config.hpp>
-#if !defined(BOOST_NO_CXX11_ALLOCATOR)
+#if defined(BOOST_CORE_ALLOCATOR_DETECTION)
 #include <boost/core/alloc_construct.hpp>
 #include <boost/core/lightweight_test.hpp>
 
