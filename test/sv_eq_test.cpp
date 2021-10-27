585ac9ace1a73f92ab76e1c72245667c0b24e49e
Peter Dimov <pdimov@gmail.com>
2021-10-27 03:08:10 +0300

    Move string_view.hpp to boost/core/detail

--- test/sv_eq_test.cpp
+++ test/sv_eq_test.cpp
@@ -2,7 +2,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#include <boost/core/string_view.hpp>
+#include <boost/core/detail/string_view.hpp>
 #include <boost/core/lightweight_test.hpp>
 #include <cstddef>
 #if !defined(BOOST_NO_CXX17_HDR_STRING_VIEW)
--- test/sv_find_first_of_test.cpp
+++ test/sv_find_first_of_test.cpp
@@ -2,7 +2,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#include <boost/core/string_view.hpp>
+#include <boost/core/detail/string_view.hpp>
 #include <boost/core/lightweight_test.hpp>
 #include <algorithm>
 #include <cstddef>
