7b45315af1f47708a045fd86dce4988fd54f63ed
Peter Dimov <pdimov@gmail.com>
2021-11-01 04:15:33 +0200

    Avoid C4996 in sv_copy_test.cpp

--- test/sv_copy_test.cpp
+++ test/sv_copy_test.cpp
@@ -2,6 +2,8 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
+#define _SCL_SECURE_NO_WARNINGS
+
 #include <boost/core/detail/string_view.hpp>
 #include <boost/core/lightweight_test.hpp>
 #include <stdexcept>
