ba208ad7fc968756b025400881c441e1bb988bb3
Peter Dimov <pdimov@gmail.com>
2021-11-01 02:20:26 +0200

    Avoid signed/unsigned mismatch warning in span.hpp

--- include/boost/core/span.hpp
+++ include/boost/core/span.hpp
@@ -15,7 +15,7 @@ Distributed under the Boost Software License, Version 1.0.
 
 namespace boost {
 
-constexpr std::size_t dynamic_extent = -1;
+constexpr std::size_t dynamic_extent = static_cast<std::size_t>( -1 );
 
 template<class T, std::size_t E = dynamic_extent>
 class span;
