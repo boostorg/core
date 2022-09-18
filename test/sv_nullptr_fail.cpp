0ac87736f86d04fda4d628bc4a55a53447c8e135
Peter Dimov <pdimov@gmail.com>
2022-09-18 21:10:18 +0300

    Explicitly disable sv_nullptr_fail when BOOST_NO_CXX11_NULLPTR is defined, because libc++ seems to have nullptr even in C++03 mode

--- test/sv_nullptr_fail.cpp
+++ test/sv_nullptr_fail.cpp
@@ -3,8 +3,17 @@
 // https://www.boost.org/LICENSE_1_0.txt
 
 #include <boost/core/detail/string_view.hpp>
+#include <boost/config.hpp>
+
+#if defined(BOOST_NO_CXX11_NULLPTR)
+
+#error BOOST_NO_CXX11_NULLPTR is defined, test will be skipped.
+
+#else
 
 int main()
 {
     return boost::core::string_view( nullptr ).size() == 0;
 }
+
+#endif
