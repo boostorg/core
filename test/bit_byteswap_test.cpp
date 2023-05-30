b7f7eb4f908d29bcbaf394516e2574b7141a9069
Peter Dimov <pdimov@gmail.com>
2023-05-30 19:27:42 +0300

    Avoid -Wlong-long under C++03

--- test/bit_byteswap_test.cpp
+++ test/bit_byteswap_test.cpp
@@ -19,8 +19,8 @@ int main()
     BOOST_TEST_EQ( boost::core::byteswap( (boost::int32_t)0x01020304 ), 0x04030201 );
     BOOST_TEST_EQ( boost::core::byteswap( (boost::uint32_t)0xF1E2D3C4u ), 0xC4D3E2F1u );
 
-    BOOST_TEST_EQ( boost::core::byteswap( (boost::int64_t)0x0102030405060708ll ), 0x0807060504030201ll );
-    BOOST_TEST_EQ( boost::core::byteswap( (boost::uint64_t)0xF1E2D3C4B5A69788ull ), 0x8897A6B5C4D3E2F1ull );
+    BOOST_TEST_EQ( boost::core::byteswap( (boost::int64_t)0x01020304 << 32 | 0x05060708 ), (boost::int64_t)0x08070605 << 32 | 0x04030201 );
+    BOOST_TEST_EQ( boost::core::byteswap( (boost::uint64_t)0xF1E2D3C4u << 32 | 0xB5A69788u ), (boost::uint64_t)0x8897A6B5u << 32 | 0xC4D3E2F1u );
 
     return boost::report_errors();
 }
